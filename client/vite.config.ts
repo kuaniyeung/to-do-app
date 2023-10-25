e550705f6f3e89ec8b7c1c7e0bbed646d659a59f
kuaniyeung@gmail.com <kuaniyeung@gmail.com>
2023-10-25 16:10:35 -0400

    Correct config file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig, } from 'vite'
 import react from '@vitejs/plugin-react'
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd());
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,15 +13,7 @@ export default defineConfig({
     port: 5050,
   },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        globals: {
-          "import.meta.env.VITE_SUPABASE_KEY": JSON.stringify(
-            process.env.SUPABASE_KEY
-          ),
-        },
-      },
-    },
+  define: {
+    "import.meta.env.VITE_SUPABASE_KEY": JSON.stringify(env.VITE_SUPABASE_KEY),
   },
 });
